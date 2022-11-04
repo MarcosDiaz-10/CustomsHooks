25bfac79e698524393263778547adf17b0fe3397
MarcosDiaz-10 <Tornadodiazelhalabi@gmail.com>
2022-11-04 02:29:11 -0400

    Actualizacion del useForm agregando la memorizacion del initialForm y evitando el error del bucle de renderizado

--- useForm/useForm.js
+++ useForm/useForm.js
@@ -1,80 +1,77 @@
 import { useEffect, useMemo, useState } from "react";
 
-
-
-
 export const useForm = ( initialForm = {}, formValidations = {}) => {
 
-   const initialFormMemo = useMemo(() => ({ ...initialForm }), [initialForm]);
-    
-    const [formState, setFormState] = useState( initialFormMemo );
-    const [formValidation, setFormValidation] = useState({})
+  const initialFormMemo = useMemo(() => ({ ...initialForm }), []);
+   
+   const [formState, setFormState] = useState( initialFormMemo );
+   const [formValidation, setFormValidation] = useState({})
 
-    useEffect(() => {
-      
-      createValidators();
-      
-    }, [ formState ])
-    
-    useEffect(() => {
+   useEffect(() => {
+     
+     createValidators();
+     
+   }, [ formState ])
+   
+   useEffect(() => {
 
-      setFormState( initialForm )
-      
-    }, [initialForm])
-    
+     setFormState( initialFormMemo )
+     
+   }, [initialFormMemo])
+   
 
-    const isFormValid = useMemo(() => { 
+   const isFormValid = useMemo(() => { 
 
-        for (const formField of Object.keys( formValidation )) {
-          
-          if( formValidation[formField] !== null) return false;
+       for (const formField of Object.keys( formValidation )) {
+         
+         if( formValidation[formField] !== null) return false;
 
-        }
+       }
 
-        return true
-     }, [ formValidation ])
-
-
-    const onInputChange = ( { target } ) => {
-        const { name, value } = target;
-        
-       setFormState({...formState, [ name ]: value });
-
-    };
-
-    const onResetForm = () => {
-        setFormState( initialForm )
-    }
-
-    const onCleanInput = ({ target }) => {
-      const { name } = target;
-        
-      setFormState({...formState, [ name ]: '' });
-    }
-
-    const createValidators = () => {
-
-      const formCheckedValues = {};
-
-      for (const formField of Object.keys( formValidations )) {
-        const [ fn, errorMessage ] = formValidations[formField]
-        
-        formCheckedValues[`${ formField }Valid`] = fn( formState[formField] ) ? null : errorMessage;
-      }
-      setFormValidation( formCheckedValues )
-      
-    };
+       return true
+    }, [ formValidation ])
 
 
-  return {
-    ...formValidation,
-    ...formState,
-    isFormValid,
-    formValidation,
-    formState,
-    onInputChange,
-    onResetForm,
-    onCleanInput
+   const onInputChange = ( { target } ) => {
+       const { name, value } = target;
+       
+      setFormState({...formState, [ name ]: value });
 
-  } 
+   };
+
+   const onResetForm = () => {
+       setFormState( initialFormMemo )
+   }
+
+   const onCleanInput = ({ target }) => {
+     const { name } = target;
+       
+     setFormState({...formState, [ name ]: '' });
+   }
+
+   const createValidators = () => {
+
+     const formCheckedValues = {};
+
+     for (const formField of Object.keys( formValidations )) {
+       const [ fn, errorMessage ] = formValidations[formField]
+       
+       formCheckedValues[`${ formField }Valid`] = fn( formState[formField] ) ? null : errorMessage;
+     }
+     setFormValidation( formCheckedValues )
+     
+   };
+
+
+ return {
+   ...formValidation,
+   ...formState,
+   isFormValid,
+   formValidation,
+   formState,
+   onInputChange,
+   onResetForm,
+   onCleanInput
+
+ } 
 }
